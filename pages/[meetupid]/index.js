7bfa55784bb2d8d7d1aedbc43241dec7dbf3b9c2
mayankvaidya436 <mayankvaidya436@gmail.com>
2024-01-31 10:22:58 +0530

    app-nextjs

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,10 @@
+import Meetupdetail from "@/components/meetups/MeetupDetail";
+
+function Meetupdetails() {
+      return <Meetupdetail 
+    title='A First  Meetup'
+    image='https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg'
+    address='Some address'
+    description='This is first meetup' />
+}
+export default Meetupdetails;
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+
+const DUMMY_MEETUP_DATA = [
+  {
+    id:'m1',
+    title:'A First  Meetup',
+    image:'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg',
+    address:'Some address',
+    description:'This is first meetup',
+  },
+  {
+    id:'m2',
+    title:'A Second  Meetup',
+    image:'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg',
+    address:'Some address',
+    description:'This is second meetup',
+  },
+  {
+    id:'m3',
+    title:'A Third  Meetup',
+    image:'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg',
+    address:'Some address',
+    description:'This is third meetup',
+  }
+]
+
+function HomePage(){
+    return  <MeetupList meetups={DUMMY_MEETUP_DATA} />
+  }
+  export default HomePage;
